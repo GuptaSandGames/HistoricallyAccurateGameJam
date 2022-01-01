46828142e4489188f3551b2688a7a522f77cbd51
AlinMGupta <guptalin@gmail.com>
2022-01-01 11:49:30 -0500

    stuff (player images)

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSystem : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int GetInventoryLevel(string name)
+    {
+      // switch (name)
+      // {
+      // }
+      return 0;
+    }
+}

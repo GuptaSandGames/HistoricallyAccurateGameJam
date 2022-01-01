0c961a8bc0698d1b7b021865d97a64e824c1588a
Kettler88 <Kettler@gupta-tech.com>
2022-01-01 12:28:54 -0500

    player controller kinda

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,7 @@ public class PlayerMove : MonoBehaviour{
 
   float horizontal;
   float vertical;
-  float moveLimiter = 0.7f;
-
-  public float runSpeed = 20.0f;
+  public float runSpeed = 5000.0f;
 
   void Start ()
   {
@@ -20,14 +18,8 @@ public class PlayerMove : MonoBehaviour{
      horizontal = Input.GetAxisRaw("Horizontal");
      vertical = Input.GetAxisRaw("Vertical");
   }
-  void FixedUpdate()
+  private void FixedUpdate()
   {
-     if (horizontal != 0 && vertical != 0)
-     {
-        horizontal *= moveLimiter;
-        vertical *= moveLimiter;
-     }
-     body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
-  }
-
+   body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+ }
 }

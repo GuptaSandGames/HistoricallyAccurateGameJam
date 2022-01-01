f5e8a89bca2f7cd537c558096d09ef6f8dc034d6
AlinMGupta <guptalin@gmail.com>
2022-01-01 10:08:07 -0500

    Added Stuff

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Button play;
+    public Button quit;
+    // Start is called before the first frame update
+    void Start()
+    {
+      play.onClick.AddListener(Play);
+      quit.onClick.AddListener(Quit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Play()
+    {
+      Debug.Log("PLAYING NOW");
+    }
+
+    public void Quit()
+    {
+
+    }
+}

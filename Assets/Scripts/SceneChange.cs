5c8b57c8616f1f7ebe787f5dd9e23a158f83485d
Kettler88 <Kettler@gupta-tech.com>
2022-01-01 18:14:53 -0500

    Scene Changes

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneChange : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class SceneChange : MonoBehaviour{
+  [SerializeField] private string nextScene;
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if(other.CompareTag("Player"))
     {
-
+      SceneManager.LoadScene(nextScene);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+  }
 }

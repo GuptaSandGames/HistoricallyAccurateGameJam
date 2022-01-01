3a102315f36a9b360385c72f9a38e7dee9eb04b1
AlinMGupta <guptalin@gmail.com>
2022-01-01 10:15:33 -0500

    Play button swaps to next scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
@@ -23,7 +23,7 @@ public class MainMenu : MonoBehaviour
 
     public void Play()
     {
-      Debug.Log("PLAYING NOW");
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Quit()

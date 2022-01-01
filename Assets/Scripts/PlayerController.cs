795d7d6d991e3fa2263a573b223c8c95dcde5000
AlinMGupta <guptalin@gmail.com>
2022-01-01 17:00:14 -0500

    Animated the Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,24 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour{
-  Rigidbody2D body;
+public class PlayerController : MonoBehaviour
+{
+  public float moveSpeed = 5f;
+  public Transform movePoint;
 
-  public float horizontal;
-  public float vertical;
-  public float runSpeed = 5000.0f;
+  public Animator anim;
+
+  public LayerMask whatStopsMovement;
+
+  public float xdist;
+  public float ydist;
 
   void Start ()
   {
-     body = GetComponent<Rigidbody2D>();
+    movePoint.parent = null;
   }
   void Update()
   {
-     horizontal = Input.GetAxisRaw("Horizontal");
-     vertical = Input.GetAxisRaw("Vertical");
+    xdist = movePoint.transform.position.x - gameObject.transform.position.x;
+    ydist = movePoint.transform.position.y - gameObject.transform.position.y;
+    transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+    if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
+    {
+      if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+      {
+        if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f),.2f,whatStopsMovement))
+        {
+          movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+        }
+      }
+
+      if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+      {
+        if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f),.2f,whatStopsMovement))
+        {
+          movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+        }
+      }
+
+      anim.SetBool("Forward", false);
+      anim.SetBool("Backward", false);
+      anim.SetBool("Left", false);
+      anim.SetBool("Right", false);
+    } else {
+      if (ydist > .001) {
+        anim.SetBool("Backward",true);
+      }
+      if (ydist < -.001) {
+        anim.SetBool("Forward",true);
+      }
+      if (xdist > .001) {
+        anim.SetBool("Right",true);
+      }
+      if (xdist < -.001) {
+        anim.SetBool("Left",true);
+      }
+    }
   }
-  private void FixedUpdate()
-  {
-   body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
- }
 }

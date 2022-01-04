bcbcb0a484329e2d648ef3263b5dc3e14f251f54
AlinMGupta <guptalin@gmail.com>
2022-01-04 16:28:00 -0500

    dhfgfdgyht6yrfvgbhbgvgbcvbvgcgbvcbgvccgv fvcgbhvmnhj vf xcdbuni8y
    esd xxujgynbvhfgtyrf5ruiy8 o9izdr8e5p]0df;lkxcv,.m xnjbhku yi98o09p-vpl;k,.km5er,tylgl;.po,,kmjnhb vcx bnmj,k.//.;l,kmjnb

--- Assets/Scripts/ItemSnap.cs
+++ Assets/Scripts/ItemSnap.cs
@@ -7,16 +7,22 @@ using UnityEngine.EventSystems;
 public class ItemSnap : MonoBehaviour, IDropHandler {
   public string hasLiquid;
   public string[] accept;
-  
+  public GameObject inMe;
+
   public void OnDrop(PointerEventData eventData) {
     if (eventData.pointerDrag != null) {
       if (Array.Exists(accept, x => x == eventData.pointerDrag.name)) {
         if (eventData.pointerDrag.name == "Spoooooon") {
+          if (gameObject.name == "honeypot") {
+            eventData.pointerDrag.GetComponent<Spoooooon>().changeSpoon("Honey");
+          }
           eventData.pointerDrag.transform.position = eventData.pointerDrag.GetComponent<DragDrop>().startPos;
-          eventData.pointerDrag.GetComponent<Spoooooon>().changeSpoon("Honey");
         } else if (eventData.pointerDrag.name == "honeySpoon") {
+          if (gameObject.name == "Mortar2") {
+            eventData.pointerDrag.GetComponent<Spoooooon>().changeSpoon("Sad");
+            hasLiquid = "honey";
+          }
           eventData.pointerDrag.transform.position = eventData.pointerDrag.GetComponent<DragDrop>().startPos;
-          eventData.pointerDrag.GetComponent<Spoooooon>().changeSpoon("Sad");
         } else {
           eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
         }

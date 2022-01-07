6d4fe027f8faeefc05d7b2da23575b03f07b7b3c
AlinMGupta <guptalin@gmail.com>
2022-01-07 15:51:47 -0500

    Revert "Music and SFX"
    This reverts commit 430d5d71b476d953858e426cfd1d2f3398a87bfe.

--- Assets/Scripts/Pestle.cs
+++ Assets/Scripts/Pestle.cs
@@ -33,13 +33,6 @@ public class Pestle : MonoBehaviour
         anim.ResetTrigger("Left");
       }
     }
-    //Pestle Sound
-    void pestle(){
-     AkSoundEngine.PostEvent("Mortar", gameObject);
-    }
-    void pestlestop(){
-           AkSoundEngine.PostEvent("Mortar_stop", gameObject);
-    }
 
 
 }
